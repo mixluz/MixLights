4982e165c8a915405a735a00409b56a073aa091f
Lukas Zeller <luz@plan44.ch>
2011-07-15 11:52:01 +0200

    New default IP for DALI bridge, updated provisioning profile settings, removed AdHoc build settings

--- Classes/MixLightsAppDelegate.m
+++ Classes/MixLightsAppDelegate.m
@@ -33,7 +33,7 @@
 	// init the defaults
   [[NSUserDefaults standardUserDefaults] registerDefaults:
   	[NSDictionary dictionaryWithObjectsAndKeys:
-    	@"192.168.59.50", @"DaliBridgeIP", // Digi Connect ME in MixWerk LAN (new VLAN: 59, old VLAN: 23)
+    	@"192.168.59.200", @"DaliBridgeIP", // Digi Connect ME in MixWerk LAN (new VLAN: 59)
     	nil
     ]
   ];
